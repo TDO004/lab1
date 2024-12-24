6ce310ed2140ebfb3bb426061fe51897d9adb850
tarasov.do <tarasov.do@dozen.mephi.ru>
2024-12-24 17:24:06 +0300

    Добавьте файлы проекта.

---
+++ cpu_load.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    while (true) {
+        auto now = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::seconds>(now - start);
+        if (duration.count() >= 10) {  // Время выполнения — 10 секунд
+            std::cout << "Program executed for " << duration.count() << " seconds.\n";
+            break;
+        }
+    }
+    return 0;
+}
